130606b47bac8b850948da8f8e1b61e00e4dc629
divyanshi-sharma <57505733+divyanshi-sharma@users.noreply.github.com>
2020-08-13 10:32:02 -0500

    update searchRouter.js

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -1,6 +1,7 @@
-import express from "express"
+import express from 'express'; //Research a way to do it just once for all files?
+import {search} from "../controllers/searchController.js"
+import cors from 'cors'; //Security measures
 
-const searchRoute = express.Router();
-searchRoute.get('/search', searchFunction)
-
-module.exports
+const searchRouter = express.Router();
+searchRouter.get('/search', search)
+export default searchRouter;
